538ab22ccee5d29b5bd04ada7554cb66e3cc98e8
rsolomon <rsolomon@webmd.net>
2024-07-01 10:32:23 -0400

    updated tailwind - css animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
         flex-col overflow-hidden
         mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1>Rodney Solomon | Portfolio</h1>
+        <Hero />
       </div>
     </main>
   );
